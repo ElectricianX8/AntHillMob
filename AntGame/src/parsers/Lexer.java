f012a7d7669df87681cf3fe10e5ac5e785dbcb2e
Oliver Thomas <olliethomas86@gmail.com>
2016-05-02 15:01:59 +0100

    AntBrain Lexer/Parser works for standard format ant brains
    Not sure why the project.properties file has been changed. Maybe don’t
    pull that in? The only difference is getting the name of the brain from
    the file path for brains that haven’t been designed by our
    (hypothetical) ant brain generator. Keeping the “~BrainName” format
    allows us to potentially add this in the future whilst being invisible
    to users who’ve brought along their own brains :)

--- AntGame/src/parsers/Lexer.java
+++ AntGame/src/parsers/Lexer.java
@@ -7,8 +7,10 @@ package parsers;
 
 
 import java.io.StringReader;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import tokens.Token;
+import tokens.TokenName;
 
 /**
  *
@@ -28,7 +30,7 @@ public class Lexer {
      * @return A list of tokens containing brain instructions.
      * @throws LexerException Invalid structure.
      */
-    public ArrayList<Token> lexAntBrain(String input) throws LexerException{
+    public ArrayList<Token> lexAntBrain(Path fileName, String input) throws LexerException{
         try {
             ArrayList<Token> lexedInstructions = new ArrayList<>();
             
@@ -37,6 +39,9 @@ public class Lexer {
             while(nextResult != null) {
                 lexedInstructions.add(nextResult);
                 nextResult = gabl.yylex();
+            }
+            if (!(lexedInstructions.get(0) instanceof TokenName)) {
+                lexedInstructions.add(0, new TokenName("~"+fileName.toString()));
             }
             return lexedInstructions;
         } catch (Exception | Error e) {
